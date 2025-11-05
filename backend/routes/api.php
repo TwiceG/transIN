c3cc5f01822736967108d867e29505242decd53c
TwiceG <g0stron0g@gmail.com>
2025-11-05 16:08:56 +0100

    add login and delivery-job-list to api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Enums\UserRole;
+use App\Http\Controllers\DeliveryJobController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/login', [UserController::class, 'login']);
+
+Route::middleware(['auth:sanctum', 'role:' . UserRole::ADMIN->value])
+    ->get('/delivery-job-list', [DeliveryJobController::class, 'listJobs']);
